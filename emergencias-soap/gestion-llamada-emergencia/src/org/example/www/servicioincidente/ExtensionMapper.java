c1c1fadd4a14eca3f4805d5c21dc5f805afa699e
Morgana <mmr209@alu.ua.es>
2026-05-19 17:13:01 +0200

    He subido lo mio de SOAP

---
+++ emergencias-soap/gestion-llamada-emergencia/src/org/example/www/servicioincidente/ExtensionMapper.java
@@ -0,0 +1,78 @@
+
+/**
+ * ExtensionMapper.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:18:31 BST)
+ */
+
+        
+            package org.example.www.servicioincidente;
+        
+            /**
+            *  ExtensionMapper class
+            */
+            @SuppressWarnings({"unchecked","unused"})
+        
+        public  class ExtensionMapper{
+
+          public static java.lang.Object getTypeObject(java.lang.String namespaceURI,
+                                                       java.lang.String typeName,
+                                                       javax.xml.stream.XMLStreamReader reader) throws java.lang.Exception{
+
+              
+                  if (
+                  "http://www.example.org/tipos".equals(namespaceURI) &&
+                  "EstadoIncidente".equals(typeName)){
+                   
+                            return  org.example.www.tipos.EstadoIncidente.Factory.parse(reader);
+                        
+
+                  }
+
+              
+                  if (
+                  "http://www.example.org/tipos".equals(namespaceURI) &&
+                  "NivelUrgencia".equals(typeName)){
+                   
+                            return  org.example.www.tipos.NivelUrgencia.Factory.parse(reader);
+                        
+
+                  }
+
+              
+                  if (
+                  "http://www.example.org/tipos".equals(namespaceURI) &&
+                  "TipoIncidente".equals(typeName)){
+                   
+                            return  org.example.www.tipos.TipoIncidente.Factory.parse(reader);
+                        
+
+                  }
+
+              
+                  if (
+                  "http://www.example.org/tipos".equals(namespaceURI) &&
+                  "Incidente".equals(typeName)){
+                   
+                            return  org.example.www.tipos.Incidente.Factory.parse(reader);
+                        
+
+                  }
+
+              
+                  if (
+                  "http://www.example.org/tipos".equals(namespaceURI) &&
+                  "Aviso".equals(typeName)){
+                   
+                            return  org.example.www.tipos.Aviso.Factory.parse(reader);
+                        
+
+                  }
+
+              
+             throw new org.apache.axis2.databinding.ADBException("Unsupported type " + namespaceURI + " " + typeName);
+          }
+
+        }
+    
